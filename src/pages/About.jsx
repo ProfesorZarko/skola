af675cecf9b4f53ac1c6798c181d18517adaa7f8
ProfesorZarko <profesorfz2@gmail.com>
2025-05-27 23:05:57 +0200

    inicijalni komit za skolu ili gimnaziju

---
+++ src/pages/About.jsx
@@ -0,0 +1,6 @@
+export default function About() {
+  return <div><h1>About Page</h1>
+    <h1>about</h1>
+    <h2>Learn more about our application and its features!</h2>
+    </div>;
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+export default function Home() {
+  return <div>
+        <h1>Home Page</h1>
+        <h2>Welcome to the home page of our application!</h2>
+        </div>
+}
